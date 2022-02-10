223376777916a871cdb842bbc5b7a152cdaafbc9
It's Me <58089002+NabilahSharfina@users.noreply.github.com>
2022-02-10 07:54:57 +0700

    Upload practice of Modul 6

--- Praktikum/Pertemuan 4/Modul 6/app/src/main/java/com/nabilah_19104025/modul6/MainActivity.kt
+++ Praktikum/Pertemuan 4/Modul 6/app/src/main/java/com/nabilah_19104025/modul6/MainActivity.kt
@@ -2,21 +2,25 @@ package com.nabilah_19104025.modul6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.nabilah_19104025.modul6.databinding.ActivityMainBinding
+import com.nabilah_19104025.modul6.adapter.CardViewMyDataAdapter
+import com.nabilah_19104025.modul6.adapter.GridMyDataAdapter
+import com.nabilah_19104025.modul6.adapter.ListMyDataAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private val list = ArrayList<MyData>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
+        rv_mydata.setHasFixedSize(true)
         list.addAll(getListMyDatas())
         showRecyclerList()
     }
+
     fun getListMyDatas(): ArrayList<MyData> {
         val dataName = resources.getStringArray(R.array.data_name)
         val dataDescription = resources.getStringArray(R.array.data_description)
@@ -33,11 +37,47 @@ class MainActivity : AppCompatActivity() {
         return listMyData
     }
 
+    private val list = ArrayList<MyData>()
+
     private fun showRecyclerList() {
-        binding.apply {
-            rvMyData.layoutManager = LinearLayoutManager(this@MainActivity)
-            val listMyDataAdapter = ListMyDataAdapter(list)
-            rvMyData.adapter = listMyDataAdapter
+        rv_mydata.layoutManager = LinearLayoutManager(this)
+        val listMyDataAdapter = ListMyDataAdapter(list)
+        rv_mydata.adapter = listMyDataAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+            R.id.action_grid -> {
+                showRecyclerGrid()
+            }
+            R.id.action_cardview -> {
+                showRecyclerCardView()
+            }
         }
+    }
+
+    private fun showRecyclerGrid() {
+        rv_mydata.layoutManager = GridLayoutManager(this, 2)
+        val gridMyDataAdapter = GridMyDataAdapter(list)
+        rv_mydata.adapter = gridMyDataAdapter
+    }
+
+    private fun showRecyclerCardView() {
+        rv_mydata.layoutManager = LinearLayoutManager(this)
+        val cardViewMyDataAdapter = CardViewMyDataAdapter(list)
+        rv_mydata.adapter = cardViewMyDataAdapter
     }
 }
